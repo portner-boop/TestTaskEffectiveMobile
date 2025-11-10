64c026a55ef246c4f4d1828964c4da76f9e4e5da
Дамир Магдеев <damirmagdeev@MacBook-Air-Damir.local>
2025-11-10 18:52:26 +0400

    Initial commit

---
+++ src/main/java/com/example/TestTaskEffectiveMobile/TestTaskEffectiveMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestTaskEffectiveMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskEffectiveMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskEffectiveMobileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/TestTaskEffectiveMobile/TestTaskEffectiveMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestTaskEffectiveMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskEffectiveMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
